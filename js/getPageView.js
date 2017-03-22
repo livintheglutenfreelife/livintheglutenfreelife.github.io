acfd86a1506b46e74bb851bce120bf79e98c624e
Livin' the Gluten Free Life <livintheglutenfreelife@gmail.com>
2017-03-21 20:42:21 -0600

    limiting Instagram photos that are pulled in, fixing js for pageviews

--- js/getPageView.js
+++ js/getPageView.js
@@ -2,7 +2,7 @@ function loadViews(path){
     $.getJSON('/viewData.json', function(data){
     $.each(data['data'], function(index, entry){
         if(path==entry['ga-pagePath']){
-            document.getElementById('viewCount').innerHTML = entry['ga-sessions']+" views";
+            document.getElementById('viewCount').innerHTML = entry['ga-pageViews']+" views";
         }
     })
   })
